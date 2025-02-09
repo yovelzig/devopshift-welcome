928ff6d27944d2dee3c7d53ba7c5ee17a63711bf
yovelzig <yovel20031@gmail.com>
2025-02-09 20:55:36 +0200

    exam_ans

--- terraformExam/main.tf
+++ terraformExam/main.tf
@@ -1,3 +1,50 @@
+# provider "aws" {
+#   region = "us-east-1"  
+# }
+
+# resource "aws_security_group" "sg" {
+#   name        = "allow_ssh_http"
+#   description = "Allow SSH and HTTP traffic"
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_instance" "ubuntu_instance" {
+#   ami           = "ami-0c02fb55956c7d316"
+#   instance_type = "t2.micro"
+
+#   associate_public_ip_address = true
+
+#   security_groups = [aws_security_group.sg.name]
+
+#   tags = {
+#     Name = "ubuntu-instance"
+#   }
+# }
+
+# output "public_ip" {
+#   value = aws_instance.ubuntu_instance.public_ip
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
